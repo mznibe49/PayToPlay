df40d88b8c3981e26f64065dbbc8cc5f76e535dd
simo <mohammedabbas.zniber@wedia.fr>
2021-09-02 17:34:32 +0200

    adding backend file + structring frontend in another file

---
+++ backend/src/main/java/com/app/backend/BackendBaseApplication.java
@@ -0,0 +1,13 @@
+package com.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaseApplication.class, args);
+	}
+
+}
