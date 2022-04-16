9118db48287281b6a59785e8147a64e2f565c9d3
simo <mohammedabbas.zniber@wedia.fr>
2022-04-16 16:59:15 +0200

    fixing error on spring launch

--- backend/src/main/java/com/app/backend/security/WebSecurityConfig.java
+++ backend/src/main/java/com/app/backend/security/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                //.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
