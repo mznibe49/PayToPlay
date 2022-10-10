b5c15b8cd524e4548f97a363570a5b976420890d
simo <mohammedabbas.zniber@wedia.fr>
2022-10-10 20:31:09 +0200

    removing imports

--- backend/src/main/java/com/app/backend/controllers/ForgotPasswordController.java
+++ backend/src/main/java/com/app/backend/controllers/ForgotPasswordController.java
@@ -3,7 +3,6 @@ package com.app.backend.controllers;
 import com.app.backend.models.PasswordResetToken;
 import com.app.backend.models.User;
 import com.app.backend.payload.request.ForgotPasswordRequest;
-import com.app.backend.payload.request.ResetPasswordRequest;
 import com.app.backend.payload.request.UpdatePasswordRequest;
 import com.app.backend.payload.response.MessageResponse;
 import com.app.backend.repository.PasswordTokenRepository;
@@ -14,7 +13,6 @@ import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -23,9 +21,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.net.URI;
 import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -63,7 +59,6 @@ public class ForgotPasswordController {
 
             try {
                 emailService.sendForgottenPassWordEmail(email, resetLink);
-                // delete token instance in database if expiration date has passe (triggers ?? batch ??)
             } catch (Throwable t) {
                 return ResponseEntity
                         .badRequest()
