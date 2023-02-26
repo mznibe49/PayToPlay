0071c95c81236a2ec068d0bf189fe9afb96b1754
simo <simozniber2@gmail.com>
2023-02-26 21:40:20 +0100

    Removing zombie code in EmailServiceImp.java

--- backend/src/main/java/com/app/backend/services/EmailServiceImp.java
+++ backend/src/main/java/com/app/backend/services/EmailServiceImp.java
@@ -1,7 +1,6 @@
 package com.app.backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -29,7 +28,7 @@ public class EmailServiceImp {
                 "<p>You have requested to reset your password.</p>" +
                 "<p>Click the link below to change your password : </p>" +
                 "<p>(This link is available only 24 hours) => <b><a href=\"" + resetLink + "\">Change my password</a></b></p>" +
-                "<p>Ignore this email if you do remember your password, or you have node made the request</p>";
+                "<p>Ignore this email if you do remember your password, or you have not made the request</p>";
 
         helper.setSubject(subject);
         helper.setText(content, true);
@@ -37,13 +36,4 @@ public class EmailServiceImp {
         emailSender.send(message);
     }
 
-   /* public void sendSimpleMessage(
-            String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@baeldung.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        emailSender.send(message);
-    } */
 }
